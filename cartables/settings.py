44f681c4b48432b415a7692f889a20a680f660ce
rsimpore <roland.simpore@mac.com>
2020-03-27 01:37:19 +0100

    Cartables d'Afrique app, 4th commit

--- cartables/settings.py
+++ cartables/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '92+a$08_84ac&*n4xvt&cup&%ymq2=etk#df_l(w0b6t&!n2c)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["www.cartablesdafrique.com"]
 
 
 # Application definition
